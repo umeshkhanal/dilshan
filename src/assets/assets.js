26f6dfed5d769230418dea56c276ed1e8a69c4c2
Umesh Khanal <116002445+umeshkhanal@users.noreply.github.com>
2026-09-17 17:24:01 +0400

    Add files via upload

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,1 +1,192 @@
+import logo from "./logo.jpeg";
+import photo from "./photo.jpeg";
+import bg from "./bg.jpeg";
 
+//fashion
+import f1 from "./portfolio/fashion/01.mp4";
+import f2 from "./portfolio//fashion/02.mp4";
+import f3 from "./portfolio/fashion/03.mp4";
+
+//events
+import e1 from "./portfolio/events/01.mp4";
+import e2 from "./portfolio//events/02.mp4";
+import e3 from "./portfolio/events/03.mp4";
+
+import eventsP1 from "./portfolio/events/p1.jpg";
+import eventsP2 from "./portfolio/events/p2.jpg";
+import eventsP3 from "./portfolio/events/p3.jpg";
+import eventsP4 from "./portfolio/events/p4.jpg";
+import eventsP5 from "./portfolio/events/p5.jpg";
+import eventsP6 from "./portfolio/events/p6.jpg";
+import eventsP7 from "./portfolio/events/p7.jpg";
+import eventsP8 from "./portfolio/events/p8.jpg";
+
+//marketing
+import m1 from "./portfolio/marketing/01.mp4";
+import m2 from "./portfolio/marketing/02.mp4";
+import m3 from "./portfolio/marketing/03.mp4";
+import m4 from "./portfolio/marketing/04.mp4";
+
+//carcare
+import care01 from "./portfolio/carcare/01.mp4";
+import care02 from "./portfolio/carcare/02.mp4";
+import care03 from "./portfolio/carcare/03.mp4";
+import careD1 from "./portfolio/carcare/d1.jpg";
+import careD2 from "./portfolio/carcare/d2.jpg";
+import careD3 from "./portfolio/carcare/d3.jpg";
+import careD4 from "./portfolio/carcare/d4.jpg";
+import careD5 from "./portfolio/carcare/d5.jpg";
+import careD6 from "./portfolio/carcare/d6.jpg";
+
+//carrentals
+import rent01 from "./portfolio/carrentals/01.mp4";
+import rentD1 from "./portfolio/carrentals/d1.jpg";
+import rentD2 from "./portfolio/carrentals/d2.jpg";
+import rentD3 from "./portfolio/carrentals/d3.jpg";
+import rentD4 from "./portfolio/carrentals/d4.jpg";
+import rentD5 from "./portfolio/carrentals/d5.jpg";
+
+//restaurant
+import rest01 from "./portfolio/restaurant/01.mp4";
+import rest02 from "./portfolio/restaurant/02.mp4";
+
+//coffee
+
+import coffeeD1 from "./portfolio/coffee/d1.jpg";
+import coffeeD2 from "./portfolio/coffee/d2.jpg";
+import coffeeD3 from "./portfolio/coffee/d3.jpg";
+import coffeeD4 from "./portfolio/coffee/d4.jpg";
+import coffeeD5 from "./portfolio/coffee/d5.jpg";
+import coffeeD6 from "./portfolio/coffee/d6.jpg";
+import coffeeD7 from "./portfolio/coffee/d7.jpg";
+import coffeeD8 from "./portfolio/coffee/d8.jpg";
+import coffeeD9 from "./portfolio/coffee/d9.jpg";
+
+//fashion
+import fashionP1 from "./portfolio/fashion/p1.jpg";
+import fashionP2 from "./portfolio/fashion/p2.jpg";
+import fashionP3 from "./portfolio/fashion/p3.jpg";
+import fashionP4 from "./portfolio/fashion/p4.jpg";
+import fashionP5 from "./portfolio/fashion/p5.jpg";
+import fashionP6 from "./portfolio/fashion/p6.jpg";
+import fashionP7 from "./portfolio/fashion/p7.jpg";
+import fashionP8 from "./portfolio/fashion/p8.jpg";
+import fashionP11 from "./portfolio/fashion/p9.jpg";
+import fashionP12 from "./portfolio/fashion/p10.jpg";
+import fashionP13 from "./portfolio/fashion/p11.jpg";
+import fashionP14 from "./portfolio/fashion/p12.jpg";
+import fashionP15 from "./portfolio/fashion/p13.jpg";
+import fashionP16 from "./portfolio/fashion/p14.jpg";
+import fashionP17 from "./portfolio/fashion/p15.jpg";
+import fashionP9 from "./portfolio/fashion/p16.jpg";
+import fashionP10 from "./portfolio/fashion/p17.jpg";
+
+//realestate
+import realestateP1 from "./portfolio/realestate/01.jpg";
+import realestateP2 from "./portfolio/realestate/02.jpg";
+import realestateP3 from "./portfolio/realestate/03.jpg";
+import realestateP4 from "./portfolio/realestate/04.jpg";
+import realestateP5 from "./portfolio/realestate/05.jpg";
+import realestateP6 from "./portfolio/realestate/06.jpg";
+import realestateP7 from "./portfolio/realestate/07.jpg";
+import realestateP8 from "./portfolio/realestate/08.jpg";
+import realestateP9 from "./portfolio/realestate/09.jpg";
+import realestateP10 from "./portfolio/realestate/10.jpg";
+import realestateP11 from "./portfolio/realestate/11.jpg";
+import realestateP12 from "./portfolio/realestate/12.jpg";
+import realestateP13 from "./portfolio/realestate/13.jpg";
+
+export const assets = {
+  bg,
+  photo,
+  logo,
+
+  //fashion
+  f1,
+  f2,
+  f3,
+  fashionP1,
+  fashionP2,
+  fashionP3,
+  fashionP4,
+  fashionP5,
+  fashionP6,
+  fashionP7,
+  fashionP8,
+  fashionP11,
+  fashionP12,
+  fashionP13,
+  fashionP14,
+  fashionP15,
+  fashionP16,
+  fashionP17,
+  fashionP10,
+  fashionP9,
+
+  //realestate
+  realestateP1,
+  realestateP2,
+  realestateP3,
+  realestateP4,
+  realestateP5,
+  realestateP6,
+  realestateP7,
+  realestateP8,
+  realestateP10,
+  realestateP9,
+  realestateP11,
+  realestateP12,
+  realestateP13,
+  //events
+  e1,
+  e2,
+  e3,
+  eventsP1,
+  eventsP2,
+  eventsP3,
+  eventsP4,
+  eventsP5,
+  eventsP6,
+  eventsP7,
+  eventsP8,
+
+  //marketing
+  m1,
+  m2,
+  m3,
+  m4,
+  //carcare
+  care01,
+  care02,
+  care03,
+  careD1,
+  careD2,
+  careD3,
+  careD4,
+  careD5,
+  careD6,
+
+  //carrentals
+  rent01,
+  rentD1,
+  rentD2,
+  rentD3,
+  rentD4,
+  rentD5,
+
+  //restaurant
+  rest01,
+  rest02,
+
+  //coffee
+
+  coffeeD1,
+  coffeeD2,
+  coffeeD3,
+  coffeeD4,
+  coffeeD5,
+  coffeeD6,
+  coffeeD7,
+  coffeeD8,
+  coffeeD9,
+};
